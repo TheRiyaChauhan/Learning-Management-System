36c43c75f8ed0e9d7cbb1a9b833d5f9fe4a02a85
Riya Chauhan <hiriyachauhan123@gmail.com>
2025-01-22 22:32:06 +0530

    login page implemented

---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,192 @@
+//pass - jmbahglc3puYy6cH
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import {
+  Tabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger,
+} from "@/components/ui/tabs"
+import { useLoginUserMutation, useRegisterUserMutation } from "@/features/api/authApi.js"
+import { Loader2 } from "lucide-react"
+import { useEffect, useState } from "react"
+import { toast } from "sonner"
+
+const Login = () => {
+    const [signupInput,setSignupInput] = useState({
+        name: "",
+        email: "",
+        password: ""
+    })
+
+    const [loginInput,setLoginInput] = useState({
+        email: "",
+        password: ""
+    })
+
+    const [registerUser, {
+        data:registerData,
+        error:registerError,
+        isLoading:registerIsLoading,
+        isSuccess:registerIsSuccess
+    }] = useRegisterUserMutation()
+
+    const [loginUser, {
+        data:loginData,
+        error:loginError,
+        isLoading:loginIsLoading,
+        isSuccess:loginIsSuccess
+    }] =  useLoginUserMutation()
+
+    useEffect(()=>{
+      if(registerIsSuccess && registerData) {
+        toast.success(registerData.message || "SignUp Successfull")
+      }
+      if(registerError){
+        toast.error(registerData.data.message||"SignUp Failed")
+      }
+      if(loginIsSuccess && loginData) {
+        toast.success(loginData.message || "Login Successfull")
+      }
+      if(loginError){
+        toast.error(loginData.data.message||"Login Failed")
+      }
+    
+    },[registerData,loginData,registerError,loginError,registerIsLoading,loginIsLoading])
+
+    const changeInputHandler = (e,type)=>{
+        const {name,value} = e.target;
+        if(type === "signup"){
+            setSignupInput({...signupInput, [name]:value});
+        }
+        else{
+            setLoginInput({...loginInput, [name]:value})
+        }
+    }
+
+    const handleRegisteration =async(type)=> {
+        const inputData = type ==="signup"? signupInput : loginInput;
+        const action = type ==="signup"? registerUser : loginUser;
+        await action(inputData);
+    }
+
+
+
+  return (
+    <div className="flex w-full items-center justify-center"> 
+    <Tabs defaultValue="account" className="w-[400px]">
+      <TabsList className="grid w-full grid-cols-2">
+        <TabsTrigger value="SignUp">SignUp</TabsTrigger>
+        <TabsTrigger value="Login">Login</TabsTrigger>
+      </TabsList>
+      <TabsContent value="SignUp">
+        <Card>
+          <CardHeader>
+            <CardTitle>SignUp</CardTitle>
+            <CardDescription>
+             Create a new account and click signup when you're done.
+            </CardDescription>
+          </CardHeader>
+          <CardContent className="space-y-2">
+            <div className="space-y-1">
+              <Label htmlFor="name">Name</Label>
+              <Input 
+              type="text" 
+              name="name"
+              value= {signupInput.name}
+              onChange = {(e)=>changeInputHandler(e,"signup")}
+              placeholder="Eg. bansal" 
+              required={true}/>
+            </div>
+            <div className="space-y-1">
+              <Label htmlFor="name">Email</Label>
+              <Input 
+              type="email"
+              name="email" 
+              value= {signupInput.email}
+              onChange = {(e)=>changeInputHandler(e,"signup")}  
+              placeholder="Eg. bansal@gmail.com"
+              required={true}/>
+            </div>
+            <div className="space-y-1">
+              <Label htmlFor="name">Password</Label>
+              <Input 
+              type="password"
+              name="password"
+              value= {signupInput.password}
+              onChange = {(e)=>changeInputHandler(e,"signup")}   
+              placeholder="Eg. xyz"  
+              required={true}/>
+            </div>
+          </CardContent>
+          <CardFooter>
+            <Button disabled ={registerIsLoading} onClick = {()=>handleRegisteration("signup")}>
+            {
+            registerIsLoading? (
+                <>
+                <Loader2 className="mr-2 h-4 w-4 animate-spin"/>Please Wait
+                </>
+            ): "SignUp"
+            }
+            </Button>
+          </CardFooter>
+        </Card>
+      </TabsContent>
+      <TabsContent value="Login">
+        <Card>
+          <CardHeader>
+            <CardTitle>Login</CardTitle>
+            <CardDescription>
+              Login your password here. After signup, you'll be logged in.
+            </CardDescription>
+          </CardHeader>
+          <CardContent className="space-y-2">
+            <div className="space-y-1">
+              <Label htmlFor="current">Email</Label>
+              <Input 
+              type="email" 
+              name="email"
+              value={loginInput.email}
+              onChange = {(e)=>changeInputHandler(e,"login")}
+              placeholder="Eg. bansal@gmail.com"  
+              required={true}/>
+            </div>
+            <div className="space-y-1">
+              <Label htmlFor="new">Password</Label>
+              <Input 
+              type="password" 
+              name="password"
+              value={loginInput.password}
+              onChange = {(e)=>changeInputHandler(e,"login")} 
+              placeholder="Eg. xyz" 
+              required={true}/>
+            </div>
+          </CardContent>
+          <CardFooter>
+            <Button disabled ={loginIsLoading} onClick = {()=>handleRegisteration("login")}>
+            {
+            loginIsLoading ? (
+                <>
+                <Loader2 className="mr-2 h-4 w-4 animate-spin"/>Please Wait
+                </>
+            ): "Login"
+            }
+            </Button>
+          </CardFooter>
+        </Card>
+      </TabsContent>
+    </Tabs>   
+    </div>
+   )
+}
+
+export default Login;
---
+++ server/index.js
@@ -0,0 +1,33 @@
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser"
+import cors from "cors"
+import connectDB from "./database/db.js";
+import userRoute from "./routes/user.route.js"
+import morgan from "morgan";
+
+dotenv.config();
+
+// call to database connection 
+connectDB();
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+//default middleware
+app.use(express.json())
+app.use(cookieParser())
+app.use(cors({
+    origin:"http://localhost:5173",
+    credentials:true
+}))
+
+app.use(morgan('dev'));
+
+//apis
+app.use("/api/v1/user", userRoute)
+
+app.listen(PORT,()=>{
+    console.log(`Server listen at port ${PORT}`);
+})
